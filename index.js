5f09ab5c61433d75e1c1948ca3d14c7603c7d17f
SopianWebDev <156198454+SopianWebDev@users.noreply.github.com>
2024-01-29 17:34:40 +0700

    Update index.js
    update responsive

--- index.js
+++ index.js
@@ -1,43 +1,51 @@
 const dataNamaSiswaKey = "DATA-NAMA-SISWA-KEY";
 const dataKelasSiswaKey = "DATA-KELAS-SISWA-KEY";
-
-
 const buttonLoginSubmit = document.getElementById("loginButton");
-const correctToken = "1234";
 const popUp = document.getElementById("popUp");
 const popUpLoading = document.getElementById("popUpLoading");
 const textPopUp = document.getElementById("textPopUp");
 
+let updateToken = null;
+const request = async () => {
+  popUpLoading.classList.remove("hidden");
+  const data = await axios("https://script.google.com/macros/s/AKfycbxpemRDCqvh1nnMdIKkh2RDFnsnlSbWkjn3T0p42ijNBsYLIWJbCEq1SqpqL25-_fQX/exec");
+  popUpLoading.classList.add("hidden");
+  console.log(data.data.data[0].Token);
+  updateToken =  data.data.data[0].Token;
+  console.log(`cek:${updateToken}`)
+};
+
+
 window.addEventListener("load", function () {
+  request()
 
-  if (localStorage.getItem(dataNamaSiswaKey)=== null) {
-    localStorage.setItem(dataNamaSiswaKey, '')
+  if (localStorage.getItem(dataNamaSiswaKey) === null) {
+    localStorage.setItem(dataNamaSiswaKey, "");
   }
 
-  if (localStorage.getItem(dataKelasSiswaKey)=== null) {
-    localStorage.setItem(dataKelasSiswaKey, '')
+  if (localStorage.getItem(dataKelasSiswaKey) === null) {
+    localStorage.setItem(dataKelasSiswaKey, "");
   }
 
-  if (localStorage.getItem(dataNamaSiswaKey) !== '' ) {
+  if (localStorage.getItem(dataNamaSiswaKey) !== "") {
     popUpLoading.classList.remove("hidden");
     setTimeout(() => {
       popUpLoading.classList.add("hidden");
-      window.location.href = "src/beranda.html";
+      window.location.href = "/src/beranda.html";
     }, 2000);
   }
-
 
   // Event untuk mengirim form
   const form = document.getElementById("my-form");
 
-  form.addEventListener("submit", function (e) {
+  form.addEventListener("submit", function async (e) {
+    console.log(`form: ${updateToken}`)
     const namaSiswa = document.getElementById("namaSiswa").value;
     const kelasSiswa = document.getElementById("Kelas").value;
     const token = document.getElementById("token").value;
 
     e.preventDefault();
-    if (namaSiswa === "" || kelasSiswa === "" || token !== correctToken) {
-      // jika nama atau kelas kosong tampilkan pop up
+    if (namaSiswa === "" || kelasSiswa === "" || token !== updateToken.toString()) {
       textPopUp.innerText = "Isi Nama, Kelas dan Masukan Token Yang Sesuai";
       popUp.classList.remove("hidden");
       setTimeout(() => {
