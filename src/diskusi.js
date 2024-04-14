12f80c6d1a12206b9fec11495c751d36ff067949
SopianWebDev <156198454+SopianWebDev@users.noreply.github.com>
2024-04-14 10:03:48 +0700

    local storage

--- src/diskusi.js
+++ src/diskusi.js
@@ -1,42 +1,61 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const AktivitasEvaluasiKey = "AKTIVITASEVALUASIKEY";
+  // inisialisasi local storage jika belum ada
+  const progresStorageKey = "Progres_Siswa";
+
+  const dataProgres = {
+    argumentasiToulmin: false,
+    pemanasanGlobal: false,
+    penyebabPemanasanGlobal: false,
+    dampakPemanasanGlobal: false,
+    solusiPemanasanGlobal: false,
+    diskusi: false,
+    evaluasi: false,
+  };
+
+  if (localStorage.getItem(progresStorageKey) === null) {
+    localStorage.setItem(progresStorageKey, JSON.stringify(dataProgres));
+  }
+
+  // Elemen
+  const evaluasi = document.querySelector(".evaluasiMenu");
+  const evaluasiLink = document.querySelector(".evaluasiMenu a");
+  const evaluasiIcon = document.querySelector(".evaluasiMenu i");
   const sudahDiskusi = document.querySelector(".sudahDiskusi");
-  const lanjutEvaluasi = document.querySelector(".lanjutEvaluasi");
-
   const navList = document.querySelector(".nav-list");
+  const lanjutEvaluasi = document.querySelector(".lanjutEvaluasi")
 
+  // Mengendalikan perilaku navlist
   window.addEventListener("load", function () {
     navList.classList.remove("initial-style");
-    renderEvaluasi();
-    if (localStorage.getItem(AktivitasEvaluasiKey) === null) {
-      localStorage.setItem(AktivitasEvaluasiKey, false);
-    }
-
-    sudahDiskusi.addEventListener("click", function () {
-      if (localStorage.getItem(AktivitasEvaluasiKey) === "false") {
-        localStorage.setItem(AktivitasEvaluasiKey, true);
-      }
-      renderEvaluasi();
-    });
+    perbaruiProgres();
   });
 
-  function renderEvaluasi() {
+  // fungsi untuk mendapatkan data dari storage
+  function getDataFromStorage() {
+    const data = localStorage.getItem(progresStorageKey);
+    return JSON.parse(data);
+  }
+
+  sudahDiskusi.addEventListener("click", function () {
+    if (getDataFromStorage().evaluasi === false) {
+      const updateProgres = getDataFromStorage();
+      updateProgres.evaluasi = true;
+      localStorage.setItem(progresStorageKey, JSON.stringify(updateProgres));
+    }
+    perbaruiProgres();
+  });
+
+  function perbaruiProgres() {
     //evaluasi
-    if (localStorage.getItem(AktivitasEvaluasiKey) === "true") {
-      const evaluasi = document.querySelector(".evaluasiMenu");
-      const evaluasiLink = document.querySelector(".evaluasiMenu a");
-      const evaluasiIcon = document.querySelector(".evaluasiMenu i");
-      const sudahDiskusi = document.querySelector('.sudahDiskusi');
-      const lanjutEvaluasi = document.querySelector('.lanjutEvaluasi');
-
-
+    if (getDataFromStorage().evaluasi === true) {
       evaluasi.classList.remove("opacity-50");
       evaluasi.classList.add("opacity-100");
 
       evaluasiLink.setAttribute("href", "/src/evaluasi.html");
       evaluasiIcon.classList.add("hidden");
       lanjutEvaluasi.classList.remove("hidden");
-      sudahDiskusi.classList.add("hidden")
+      sudahDiskusi.classList.add("hidden");
+
     }
   }
 });
