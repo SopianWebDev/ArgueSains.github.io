9546301e0f9cf8bc52337a28f29d9ec1f34b510d
SopianWebDev <156198454+SopianWebDev@users.noreply.github.com>
2024-04-14 10:02:29 +0700

    local storage

--- src/beranda.js
+++ src/beranda.js
@@ -1,94 +1,112 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // Lock Aktivitas
-  const aktivitas1Key = "AKTIVITAS1KEY";
-  const localAktivitas1 = localStorage.getItem(aktivitas1Key);
-  const aktivitas2Key = "AKTIVITAS2KEY";
-  const localAktivitas2 = localStorage.getItem(aktivitas2Key);
-  const aktivitas3Key = "AKTIVITAS3KEY";
-  const localAktivitas3 = localStorage.getItem(aktivitas3Key);
-  const aktivitas4Key = "AKTIVITAS4KEY";
-  const localAktivitas4 = localStorage.getItem(aktivitas4Key);
-  const aktivitas5Key = "AKTIVITAS5KEY";
-  const localAktivitas5 = localStorage.getItem(aktivitas5Key);
-  const AktivitasDiskusiKey = "AKTIVITASDISKUSIKEY";
-  const AktivitasDiskusi = localStorage.getItem(AktivitasDiskusiKey);
-  const AktivitasEvaluasiKey = "AKTIVITASEVALUASIKEY";
-  const AktivitasEvaluasi = localStorage.getItem(AktivitasEvaluasiKey);
+  const progresStorageKey = "Progres_Siswa";
 
+  // daftar elemen
   const heroButton = document.getElementById("heroButton");
-  const menuList1 = document.querySelector(".nav-list .aktivitas1Lock");
+  const menuList1 = document.querySelector(".nav-list .argumentasiToulminLock");
   const tombolLanjutMateri = document.getElementById("tombolLanjutMateri");
   const tombolSudahMenonton = document.getElementById("tombolSudahMenonton");
   const tombolLogout = document.getElementById("logout");
   const popUp = document.getElementById("popUp");
   const navList = document.querySelector(".nav-list");
 
+  const dataProgres = {
+    argumentasiToulmin: false,
+    pemanasanGlobal: false,
+    penyebabPemanasanGlobal: false,
+    dampakPemanasanGlobal: false,
+    solusiPemanasanGlobal: false,
+    diskusi: false,
+    evaluasi: false,
+  };
+
+  // inisialisasi local storage jika belum ada
+  if (localStorage.getItem(progresStorageKey) === null) {
+    localStorage.setItem(progresStorageKey, JSON.stringify(dataProgres));
+  }
+
+  // Animasi pada layar lebih dari 1024px
+  function setAosAttribute() {
+    const elements = document.querySelectorAll(".hero-fitur [data-aos]");
+    elements.forEach(function (element) {
+      element.setAttribute("data-aos", "fade-up");
+    });
+  }
+
+  function checkScreenWidth() {
+    let screenWidth = window.innerWidth;
+    let thresholdWidth = 1024;
+
+    if (screenWidth >= thresholdWidth) {
+      setAosAttribute();
+    }
+  }
+  checkScreenWidth();
+
+  // mengendalikan perilaku navlist
   window.addEventListener("load", function () {
     navList.classList.remove("initial-style");
-
-    tombolLogout.addEventListener("click", function () {
-      popUp.classList.remove("hidden");
-    });
-
-    const yesConfirmation = document.querySelector(".buttonYesConfirmation");
-    const noConfirmation = document.querySelector(".buttonNoConfirmation");
-
-    yesConfirmation.addEventListener("click", function () {
-      popUp.classList.add("hidden");
-      localStorage.clear();
-      window.location.href = "/index.html";
-    });
-
-    noConfirmation.addEventListener("click", function () {
-      popUp.classList.add("hidden");
-      location.reload();
-    });
-
-    // Greeting Siswa
-    const greetingSiswa = document.querySelectorAll(".greetingSiswa");
-    greetingSiswa.forEach(function (elemen) {
-      elemen.innerText = localStorage.getItem("DATA-NAMA-SISWA-KEY");
-      elemen.classList.add("text-amber-400");
-    });
-
-    renderAktivitas();
-    if (localAktivitas1 === null) {
-      localStorage.setItem(aktivitas1Key, false);
-    }
-
-    if (localAktivitas2 === null) {
-      localStorage.setItem(aktivitas2Key, false);
-    }
-
-    if (localAktivitas3 === null) {
-      localStorage.setItem(aktivitas3Key, false);
-    }
-    if (localAktivitas4 === null) {
-      localStorage.setItem(aktivitas4Key, false);
-    }
-    if (localAktivitas5 === null) {
-      localStorage.setItem(aktivitas5Key, false);
-    }
-    if (AktivitasDiskusi === null) {
-      localStorage.setItem(AktivitasDiskusiKey, false);
-    }
-    if (AktivitasEvaluasi === null) {
-      localStorage.setItem(AktivitasEvaluasiKey, false);
-    }
-
-    tombolSudahMenonton.addEventListener("click", function () {
-      if (localStorage.getItem(aktivitas1Key) === "false") {
-        localStorage.setItem(aktivitas1Key, true);
-        renderAktivitas();
-      }
-    });
   });
 
-  function renderAktivitas() {
-    if (localStorage.getItem(aktivitas1Key) === "true") {
-      const aktivitas1IconLock = document.querySelectorAll(".aktivitas1Lock i");
-      const aktivitas1Link = document.querySelectorAll(".aktivitas1Lock");
-      aktivitas1IconLock.forEach(function (element) {
+  // memperbarui progres ketika pertama kali dibuka
+  perbaruiProgres();
+
+  // logout
+  tombolLogout.addEventListener("click", function () {
+    popUp.classList.remove("hidden");
+  });
+  const yesConfirmation = document.querySelector(".buttonYesConfirmation");
+  const noConfirmation = document.querySelector(".buttonNoConfirmation");
+
+  yesConfirmation.addEventListener("click", function () {
+    popUp.classList.add("hidden");
+    localStorage.removeItem("biodata");
+    window.location.href = "/index.html";
+  });
+
+  noConfirmation.addEventListener("click", function () {
+    popUp.classList.add("hidden");
+    location.reload();
+  });
+
+  // Greeting user
+  try {
+    const greetingSiswa = document.querySelectorAll(".greetingSiswa");
+    greetingSiswa.forEach(function (elemen) {
+      elemen.innerText = JSON.parse(localStorage.getItem("biodata")).nama;
+      elemen.classList.add("text-amber-400");
+    });
+  } catch (err) {
+    console.log(err);
+  }
+
+  // Apakah user sudah menonton tutorial?
+  tombolSudahMenonton.addEventListener("click", function () {
+    if (getDataFromStorage().argumentasiToulmin === false) {
+      const data = getDataFromStorage();
+      data.argumentasiToulmin = true;
+      console.log(data);
+      localStorage.setItem(progresStorageKey, JSON.stringify(data));
+    }
+    location.reload();
+    setTimeout(function () {
+      perbaruiProgres();
+    }, 1500);
+  });
+
+  // fungsi untuk mendapatkan data dari storage
+  function getDataFromStorage() {
+    const data = localStorage.getItem(progresStorageKey);
+    return JSON.parse(data);
+  }
+
+  // fungsi untuk memperbarui progres dan tampilan
+  function perbaruiProgres() {
+    // argumentasi toulmin
+    if (getDataFromStorage().argumentasiToulmin === true) {
+      const argumentasiToulminLock = document.querySelectorAll(".argumentasiToulminLock i");
+      const argumentasiToilminLink = document.querySelectorAll(".argumentasiToulminLock");
+      argumentasiToulminLock.forEach(function (element) {
         element.classList.add("hidden");
       });
 
@@ -98,7 +116,7 @@ document.addEventListener("DOMContentLoaded", function () {
       menuList1.classList.remove("opacity-50");
       menuList1.classList.add("opacity-100");
 
-      aktivitas1Link.forEach(function (elemen) {
+      argumentasiToilminLink.forEach(function (elemen) {
         elemen.setAttribute("href", "Argumentasi/Argumentasi.html");
       });
 
@@ -107,7 +125,7 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     //  Diskusi
-    if (localStorage.getItem(AktivitasDiskusiKey) === "true") {
+    if (getDataFromStorage().diskusi === true) {
       const diskusi = document.querySelector(".diskusiMenu");
       const diskusiLink = document.querySelector(".diskusiMenu a");
       const diskusiIcon = document.querySelector(".diskusiMenu i");
@@ -120,7 +138,7 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     //evaluasi
-    if (localStorage.getItem(AktivitasEvaluasiKey) === "true") {
+    if (getDataFromStorage().evaluasi === true) {
       const evaluasi = document.querySelector(".evaluasiMenu");
       const evaluasiLink = document.querySelector(".evaluasiMenu a");
       const evaluasiIcon = document.querySelector(".evaluasiMenu i");
